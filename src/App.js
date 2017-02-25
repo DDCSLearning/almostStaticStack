32513faaba2bc4712beb724b617c3cef4ac3dbe3
Daniel Deutsch <deudan1010@gmail.com>
2017-02-25 16:42:57 +0100

    add styled components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import Helmet from 'react-helmet';
+
 import logo from './logo.svg';
 import './App.css';
+import Style from './Style';
+
 
 class App extends Component {
   render() {
@@ -16,11 +19,11 @@ class App extends Component {
         </div>
         <blockquote
           cite="https://medium.com/superhighfives/an-almost-static-stack-6df0a2791319#.mqz71luk6"
-        ><p>
+        ><Style>
           I’m a big fan of create-react-app. It hugely simplifies starting a new React-based project, and it serves pretty well for any ES6 / ES2015 setup regardless of whether you use React. I originally built Sandpit with it, purely because I could get straight to coding, writing tests, and deploying to a static site host like Surge or Now.
 
           create-react-app’s best feature is its simplicity, but that that simplicity comes at a cost. As your application grows in complexity, you either have to yarn eject and take the entire set up into your own hands, or live with the limitations.
-          </p>
+        </Style>
         </blockquote>
       </div>
     );
---
+++ src/Style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Style = styled.p`
+color: red;
+border-left: 0.3em solid black;
+`;
+
+export default Style;
