9713d40c9e509d0eebc7b93dd5e1fbcf9d3c08b4
Daniel Deutsch <deudan1010@gmail.com>
2017-02-25 12:33:04 +0100

    Add render from snapshot to index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { render } from 'react-snapshot';
+
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+render(<App />, document.getElementById('root'));
